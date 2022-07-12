c9ce859636ed53b16fe71a9b3a959e71d435b93c
Debraj Banik <writetodebrajbanik@gmail.com>
2022-07-13 01:59:36 +0530

    All subsequence patterns

--- Recursion/SubsequencePatterns.java
+++ Recursion/SubsequencePatterns.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Subsequence {
+public class SubsequencePatterns {
     
     public static void printSubsequence(int [] arr, ArrayList<Integer> ans, int size, int index){
         if(index == size){
@@ -43,6 +43,42 @@ public class Subsequence {
         sumSubsequence(arr, ans, size, index+1, sum, s);
     }
 
+    public static boolean firstSumSubsequence(int [] arr, ArrayList<Integer> ans, int size, int index, int sum, int s){
+        if(index == size){
+            if(s == sum){
+                System.out.println(ans);
+                return true;
+            }
+            return false;
+        }
+
+        ans.add(arr[index]);
+        s+=arr[index];
+
+        if(firstSumSubsequence(arr, ans, size, index+1, sum, s)==true) return true;
+
+        ans.remove(ans.size()-1);
+        s-=arr[index];
+
+        if(firstSumSubsequence(arr, ans, size, index+1, sum, s)==true) return true;
+
+        return false;
+    }
+
+    public static int countSumSubsequence(int [] arr, int size, int index, int sum, int s){
+        if(index == size){
+            if(s == sum){
+                return 1;
+            }
+            return 0;
+        }
+        s+=arr[index];
+        int l = countSumSubsequence(arr, size, index+1, sum, s);
+        s-=arr[index];
+        int r = countSumSubsequence(arr, size, index+1, sum, s);
+        return l + r;
+    }
+
     public static void main(String[] args) {
         int [] arr = {3,1,2};
         ArrayList<Integer> ans = new ArrayList<>();
@@ -51,5 +87,11 @@ public class Subsequence {
         System.out.println("------------------");
         System.out.println("Subsequence of Arr with Sum 3");
         sumSubsequence(arr, ans, 3, 0, 3, 0);
+        System.out.println("------------------");
+        System.out.println("First Subsequence of Arr with Sum 3:");
+        firstSumSubsequence(arr, ans, 3, 0, 3, 0);
+        System.out.println("------------------");
+        System.out.println("Count of Subsequences of Arr with Sum 3 : "+countSumSubsequence(arr, 3, 0, 3, 0));
+        
     }
 }
