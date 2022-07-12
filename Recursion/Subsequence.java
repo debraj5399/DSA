ab6572c651ee6ccdaa7f0aed2859d4aa5686f69e
Debraj Banik <writetodebrajbanik@gmail.com>
2022-07-13 01:42:27 +0530

    Subsequence with sum N

--- Recursion/Subsequence.java
+++ Recursion/Subsequence.java
@@ -23,9 +23,33 @@ public class Subsequence {
         ans.remove(ans.size()-1);
     }
 
+
+    public static void sumSubsequence(int [] arr, ArrayList<Integer> ans, int size, int index, int sum, int s){
+        if(index == size){
+            if(s == sum){
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        ans.add(arr[index]);
+        s+=arr[index];
+
+        sumSubsequence(arr, ans, size, index+1, sum, s);
+
+        ans.remove(ans.size()-1);
+        s-=arr[index];
+
+        sumSubsequence(arr, ans, size, index+1, sum, s);
+    }
+
     public static void main(String[] args) {
         int [] arr = {3,1,2};
         ArrayList<Integer> ans = new ArrayList<>();
+        System.out.println("All Subsequences of Arr : ");
         printSubsequence(arr, ans, 3, 0);
+        System.out.println("------------------");
+        System.out.println("Subsequence of Arr with Sum 3");
+        sumSubsequence(arr, ans, 3, 0, 3, 0);
     }
 }
